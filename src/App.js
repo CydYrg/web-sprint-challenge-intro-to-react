16082270bc04585e4b63cf471e31945cc1ae8ab0
CydYrg <ceyda_yurga@hotmail.com>
2021-05-14 21:52:22 +0100

    worked on app.js characters.js index.js. index.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect, fetchData } from "react";
 import './App.css';
+import Character from './components/Character.js';
+import axios from "axios";
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +11,36 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("react");
+  
+  const handleChange = e => {
+    setQuery(e.target.value)
+    fetchData(); // calls same external function after setting query
+  }
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://swapi.dev/api/people" + query)
+        .then(res => setData(res.data));
+    };
+    console.log(data)
+    fetchData();
+  }, [query]);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div>
+      <button onClick={e => setQuery(number + 1)}>search</button>
+      <input value={query} onChange={e => setQuery(e.target.value)} />
+      <ul>
+        {data.hits.map(item => (
+          //<li key={item.results.name}>
+          <li>
+            <Character user={data} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,11 @@
 // Write your Character component here
+const Character = (props) => {
+    return (
+        <div>
+            <h1>{props.data.name}.</h1>
+            <h2>{props.data.birth_year}.</h2>
+        </div>
+    )
+}
+
+export default Character;
